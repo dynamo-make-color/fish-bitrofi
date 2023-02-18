ecdbfa42413d44ac7bb0f638f0d6123cfe99fa16
Trần Đức Nam <dynamo.foss@gmail.com>
2023-02-19 02:31:57 +0700

    feat: get and autotype with keybinding

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ A command on fish shell to get the Bitwarden CLI functionality in Rofi menu.
 - [fisher](https://github.com/jorgebucaran/fisher)
 - [rbw](https://github.com/dynamotn/rbw): My custom version of rbw
 - [rofi](https://github.com/davatorium/rofi)
+- `xdotool`: auto typing username, password, totp
 - `glib2`: send notification
 
 Install by following command:
--- functions/__bitrofi_show_items.fish
+++ functions/__bitrofi_show_items.fish
@@ -6,11 +6,25 @@ function __bitrofi_show_items
   rbw unlock
   rbw unlocked
   if test $status -eq 0
-    rbw list | rofi -modi "password" -show password -dmenu -p "Password" -i -no-custom \
+    set -l item (rbw list | rofi -modi "password" -show password -dmenu -p "Password" -i \
       -mesg "<b>$KB_SYNC</b>: sync | <b>$KB_TOTP</b>: totp | <b>$KB_USER</b>: username" \
       -kb-custom-1 $KB_SYNC \
       -kb-custom-2 $KB_TOTP \
-      -kb-custom-3 $KB_USER
+      -kb-custom-3 $KB_USER)
+    set -l result
+    switch $status
+      case 10
+        rbw sync
+      case 11
+        set result (rbw get --field totp $item)
+      case 12
+        set result (rbw get --field user $item)
+      case '*'
+        set result (rbw get $item)
+    end
+    __bitrofi_show_error "Please remove your finger to keyboard"
+    sleep 1.2
+    echo "type $result" | xdotool -
   else
     __bitrofi_show_error "Can't unlock Bitwarden vault"
   end
